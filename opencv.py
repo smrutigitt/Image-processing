b98ec60b8b173eaada15ba8da19856a62d9fb770
smrutigitt <111482271+smrutigitt@users.noreply.github.com>
2022-08-18 10:47:28 +0530

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -1,9 +1,10 @@
+
 import cv2
-img="flower.png"
-src=cv2.imread(img)
-tmp=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-_,alpha= cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
-b,g,r=cv2.split(src)
-rgba=[b,g,r,alpha]
-dst=cv2.merge(rgba, 4)
-cv2.imwrite("gfg.png",dst)
+import numpy as np
+
+src=cv2.imread('flower.png')
+print(src.shape)
+
+src[:,:,0] = np.zeros([src.shape[0],src.shape[1]])
+
+cv2.imwrite('blue.png',src)
