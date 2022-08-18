20c012a77f9bed020359d216e7a040efb3aac6bc
smrutigitt <111482271+smrutigitt@users.noreply.github.com>
2022-08-18 19:35:05 +0530

    Update rgb.py

--- rgb.py
+++ rgb.py
@@ -1,7 +1,8 @@
+import numpy as np
 from PIL import Image
-img=Image.open('flower.png')
-
-matrix = (1,0,0,0,  0,1,0,0,  0,0,0,0)
-        
-img=img.convert("RGB",matrix)
+arr=np.zeros((2,2,3))
+#arr=np.array()
+print(arr)
+img=Image.fromarray(arr,"RGB")
 img.show()
+img.save('img.png')
