e7c35e16b3dbccf88bc2978a4550d18f1af398db
smrutigitt <111482271+smrutigitt@users.noreply.github.com>
2022-08-19 15:27:58 +0530

    Update img.py

--- img.py
+++ img.py
@@ -1,9 +1,23 @@
+from pickletools import uint8
 import numpy as np
 from PIL import Image
 
 image=Image.open('flower.jpg')
-array=np.array(image)
-print(array[10,10,:])
-image.show()
+arr=np.array(image)
+#arr[:,:]=[,,0]
+print(arr[10,10,:])
+print(arr.shape)
+print(arr.dtype)
+print(arr.size)
+print(arr)
 
-image.save('flower.png')
+# assign blue channel to zeros
+arr[:,:,2] = np.zeros([arr.shape[0], arr.shape[1]])
+
+
+img=Image.fromarray(arr)
+#img.save('flowerr.png')
+img.show()
+
+
+
